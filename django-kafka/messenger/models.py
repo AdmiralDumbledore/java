0966d48bd2ca9f82481b213696b106cb6324b6de
AdmiralDumbledore <alexanderburdinkzn@gmail.com>
2023-01-21 23:53:56 +0300

    save proj

--- django-kafka/messenger/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Message(models.Model):
-    user = models.ForeignKey('user.User', related_name='user_message', on_delete=models.CASCADE)
-    message_body = models.TextField()
-    status = models.CharField(max_length=128)
--- django-kafka/messenger/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-app_name = "messenger"
-
-urlpatterns = [
-    path('message/', SendMessage.as_view()),
-    path('message_confirmation/', SetStatus.as_view()),
-]
