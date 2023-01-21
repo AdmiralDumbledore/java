2c136af437e9828f419f736ad3a783451dd0a4ae
AdmiralDumbledore <alexanderburdinkzn@gmail.com>
2023-01-21 23:38:24 +0300

    add java proj

---
+++ src/main/java/smallest/controllers/WelcomeController.java
@@ -0,0 +1,21 @@
+package smallest.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class WelcomeController {
+	
+	@RequestMapping(method = RequestMethod.GET, produces = {"application/json"}, value = "/app")
+	public @ResponseBody String helloWorld() {
+		
+		//Flux
+		
+		
+		
+		
+		return "Hello FUCKING World!!!"; //"application/json" mean this is a text not a redirect
+	}
+}
